1dd5836b768b76f20c5035472df3254fc01b2a45
yyan-ashnaf <114198515+yyan-ashnaf@users.noreply.github.com>
2025-09-17 09:53:27 -0700

    testing initial push

---
+++ test.py
@@ -0,0 +1,39 @@
+### Use this file as a way to test other PGx data pipelines
+
+# need to figure out what packages to use
+
+
+
+
+# need to add different helper functions
+
+
+
+
+# this can be the main body for the data pipeline
+
+
+
+
+# create some helpful graphs for visual representation and analysis (proabbly just using helper functions)
+
+
+
+
+# send this over the J&J once done!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
